635c64a9e1e44101b1550871c89a8e0d9faf374d
Frederik Vollert <frederik.vollert@dynport.de>
2014-07-18 16:21:56 +0200

    adds configurable app name and issuer

--- lib/devise_google_authenticatable/controllers/helpers.rb
+++ lib/devise_google_authenticatable/controllers/helpers.rb
@@ -1,10 +1,11 @@
 module DeviseGoogleAuthenticator
   module Controllers # :nodoc:
     module Helpers # :nodoc:
-      def google_authenticator_qrcode(user,qualifier=nil)
+      def google_authenticator_qrcode(user, qualifier=nil)
         username = username_from_email(user.email)
-        app = Rails.application.class.parent_name
-        data = "otpauth://totp/#{otpauth_user(username, app, qualifier)}?secret=#{user.gauth_secret}"
+        app = Rails.configuration.google_authenticator_app_name || Rails.application.class.parent_name
+        issuer = Rails.configuration.google_authenticator_issuer
+        data = "otpauth://totp/#{otpauth_user(username, app, qualifier)}?secret=#{user.gauth_secret}&issuer=#{issuer}"
         data = Rack::Utils.escape(data)
         url = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{data}"
         return image_tag(url, :alt => 'Google Authenticator QRCode')
